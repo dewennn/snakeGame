0e6ee16bc76f6362b36e2f4f0d140a7e9cf98d76
dewen <derren9283@gmail.com>
2023-08-22 01:44:31 +0700

    menu basics

--- snakeGame.cpp
+++ snakeGame.cpp
@@ -9,28 +9,77 @@ void red () {
 void reset () {
   printf("\033[0m");
 }
+void clearScreen() {
+	system("cls");
+}
 
 void menu(){
-	char logo[7][256] = {
-		"     _______..__   __.      ___       __  ___  _______   _______      ___      .___  ___.  _______ ",
-		"    /       ||  \\ |  |     /   \\     |  |/  / |   ____| /  _____|    /   \\     |   \\/   | |   ____|",
-		"   |   (----`|   \\|  |    /  ^  \\    |  '  /  |  |__   |  |  __     /  ^  \\    |  \\  /  | |  |__   ",
-		"    \\   \\    |  . `  |   /  /_\\  \\   |    <   |   __|  |  | |_ |   /  /_\\  \\   |  |\\/|  | |   __|  ",
-		".----)   |   |  |\\   |  /  _____  \\  |  .  \\  |  |____ |  |__| |  /  _____  \\  |  |  |  | |  |____ ",
-		"|_______/    |__| \\__| /__/     \\__\\ |__|\\__\\ |_______| \\______| /__/     \\__\\ |__|  |__| |_______|"   
-	};
+	int choice = 1;
 	
-	red();
-	for(int i = 0; i < 7; i++){
-		printf("%s\n", logo[i]);
+	while(true){
+		char logo[7][256] = {
+			"     _______..__   __.      ___       __  ___  _______   _______      ___      .___  ___.  _______ ",
+			"    /       ||  \\ |  |     /   \\     |  |/  / |   ____| /  _____|    /   \\     |   \\/   | |   ____|",
+			"   |   (----`|   \\|  |    /  ^  \\    |  '  /  |  |__   |  |  __     /  ^  \\    |  \\  /  | |  |__   ",
+			"    \\   \\    |  . `  |   /  /_\\  \\   |    <   |   __|  |  | |_ |   /  /_\\  \\   |  |\\/|  | |   __|  ",
+			".----)   |   |  |\\   |  /  _____  \\  |  .  \\  |  |____ |  |__| |  /  _____  \\  |  |  |  | |  |____ ",
+			"|_______/    |__| \\__| /__/     \\__\\ |__|\\__\\ |_______| \\______| /__/     \\__\\ |__|  |__| |_______|"   
+		};
+		
+		red();
+		for(int i = 0; i < 7; i++){
+			printf("%s\n", logo[i]);
+		}
+		reset();
+		printf("\n\n");
+		
+		if(choice == 1){
+			red();
+			printf("-> Login\n");
+			reset();
+		}
+		else printf("- Login\n");
+		
+		if(choice == 2){
+			red();
+			printf("-> Register\n");
+			reset();
+		}
+		else printf("- Register\n");
+		
+		if(choice == 3){
+			red();
+			printf("-> How to Play\n");
+			reset();
+		}
+		else printf("- How to Play\n");
+		
+		if(choice == 4){
+			red();
+			printf("-> HighScore\n");
+			reset();
+		}
+		else printf("- HighScore\n");
+		
+		if(choice == 5){
+			red();
+			printf("-> Exit\n");
+			reset();
+		}
+		else printf("- Exit\n");
+		
+		int inp;
+		inp = _getch();
+		if(inp == 0 || inp == 224) inp = _getch();
+		
+		if(inp == 72 && choice - 1 >= 1){
+			choice -= 1;
+		}
+		else if(inp == 80 && choice + 1 <= 5){
+			choice += 1;
+		}
+		clearScreen();
 	}
-	reset();
-	printf("\n\n");
-	printf("1. Login\n");
-	printf("2. Register\n");
-	printf("3. How to Play\n");
-	printf("4. HighScore\n");
-	printf("5. Exit\n");
 }
 
 int main(){
